75bde20dcb3283fecbe68cd19a1d19ec0ef800ec
faizansolangi <faizansolangi576@gmail.com>
2023-12-19 14:46:54 +0500

    committed by muskan

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from "react-router-dom";
 import Home from "./screens/home";
 import GoTopButton from "./common/components/GoTopButton";
@@ -10,7 +11,6 @@ import { SignUp } from "./screens/SignUP/SignUp";
 function App() {
   return (
     <div>
-    
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
